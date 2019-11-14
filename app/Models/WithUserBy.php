397c25378af2d9577a7b78c7cb54ad3eb358c670
endropie <hendrowibowo@msn.com>
2019-11-14 17:16:30 +0700

    FIX: Update WithUserBy

--- app/Models/WithUserBy.php
+++ app/Models/WithUserBy.php
@@ -1,19 +1,13 @@
 <?php
 namespace App\Models;
 
-use function GuzzleHttp\json_decode;
-
 trait WithUserBy
 {
-    public function __construct(array $attributes = []) {
-        parent::__construct($attributes);
-    }
-
     public static function bootWithUserBy()
 	{
 		static::creating(function ($model)
         {
-            if ($user =  \Auth::user()) {
+            if ($user =  auth()->user()) {
                 $model->created_by = $user->id;
             }
             return $model;
