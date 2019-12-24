cb643496a8dcea089f6af1f4b7f20236b1cf9da3
endropie <hendrowibowo@msn.com>
2019-12-25 01:39:17 +0700

    FIX: Filter method removed (production & packing).

--- app/Filters/Factory/WorkOrder.php
+++ app/Filters/Factory/WorkOrder.php
@@ -41,32 +41,6 @@ class WorkOrder extends Filter
         return $this->builder->orWhereIn('id', $value);
     }
 
-    public function or_detail_ids($value = '') {
-        if (!strlen($value)) return $this->builder;
-
-        $value = explode(',',$value);
-        return $this->builder->orWhere(function($query) use($value){
-            $query->whereHas('work_order_items', function ($q) use($value) {
-                return $q->whereIn('id',  $value);
-            });
-        });
-    }
-
-    public function or_work_order_item_line_ids($value = '') {
-        if (!strlen($value)) return $this->builder;
-
-        $value = explode(',',$value);
-        return $this->builder->orWhere(function($query) use($value){
-            $query->whereHas('work_order_items.work_order_item_lines', function ($q) use($value) {
-                return $q->whereIn('id',  $value);
-            });
-        });
-    }
-
-    public function line_id($line) {
-        return $this->builder->where('line_id', $line);
-    }
-
     public function begin_date($value) {
         return $this->builder->where('date', '>=',  $value);
     }
@@ -79,42 +53,5 @@ class WorkOrder extends Filter
         return $this->builder->whereHas('work_order_items', function ($q) use($value) {
             return $q->where('item_id',  $value);
         });
-    }
-
-    public function has_amount_packing($value) {
-        $callback =  function ($q) {
-          $or_details = explode(',', request('or_detail_ids', ''));
-          $q->whereRaw('amount_process > amount_packing')
-            ->orWhereIn('item_id', $or_details)
-            ->whereHas('item', function ($q) {
-                if (request('customer_id')) $q->where('customer_id', request('customer_id'));
-            });
-
-        };
-        return $this->builder
-            ->with(['work_order_items'])
-            ->stateHasNot('PACKED')
-            ->whereHas('work_order_items', $callback);
-    }
-
-    public function has_amount_line($line) {
-        $keys = request('or_work_order_item_line_ids', '-1');
-        $callback = function ($q) use ($line, $keys) {
-            return $q->where('line_id', $line)
-                     ->select('work_order_item_lines.*',
-                     \DB::raw("(SELECT quantity*unit_rate as total
-                                FROM work_order_items WHERE work_order_items.id = work_order_item_lines.work_order_item_id) as amount"))
-                     ->havingRaw("amount > amount_line OR work_order_item_lines.id IN ($keys)");
-
-        };
-
-        if((int) $line) {
-            return $this->builder
-            ->where('status', '<>', 'CLOSED')
-            ->stateHasNot('PRODUCTED')
-            ->with(['work_order_item_lines' => $callback])
-            ->whereHas('work_order_item_lines', $callback);
-        }
-        // else return $this->builder;
     }
 }
