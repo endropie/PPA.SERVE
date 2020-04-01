46b66c3487bab1c5530d1fe3e17650522a2d6f10
endropie <hendrowibowo@msn.com>
2020-04-02 00:17:19 +0700

    FIX: Seeder Number index of DeliveryOrder

--- database/seeds/DeliveryOrderNumberIndexFix.php
+++ database/seeds/DeliveryOrderNumberIndexFix.php
@@ -13,13 +13,14 @@ class DeliveryOrderNumberIndexFix extends Seeder
     {
         DB::beginTransaction();
 
-        $deliveries = DeliveryOrder::where('indexed_number', 'LIKE', '%1000')->get();
+        $deliveries = DeliveryOrder::where('indexed_number', 'LIKE', '%03/1000')->get();
         foreach ($deliveries as $delivery) {
             $prefix_code = $delivery->customer->code;
             $number = $this->getNextSJDeliveryIndexedNumber($delivery->date, $prefix_code);
             $update = DeliveryOrder::withTrashed()->where('number', $delivery->number)->update(['indexed_number' => $number]);
             if ($update) {
-                print("DeliveryOrder($delivery->id) ($delivery->indexed_number) :: [$update]\n");
+                $new = DeliveryOrder::find($delivery->id);
+                print("DeliveryOrder($delivery->id) ($new->indexed_number) :: [$update]\n");
             }
             else {
                 print("DeliveryOrder($delivery->id) FAILED!\n");
