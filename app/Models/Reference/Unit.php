a8bf65ade7f6c4fac93ab7c242c4bb21256c31ad
endropie <hendrowibowo@msn.com>
2020-06-22 15:42:08 +0700

    fix(Unit): add number decimal of unit

--- app/Models/Reference/Unit.php
+++ app/Models/Reference/Unit.php
@@ -8,7 +8,7 @@ class Unit extends Model
 {
     use Filterable;
 
-    protected $fillable = ['code', 'name'];
+    protected $fillable = ['code', 'name', 'decimal_in'];
 
     protected $hidden = ['created_at', 'updated_at'];
 }
