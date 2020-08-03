ea839da4b4e61292145ae4cc05b69697ba6c1fb3
endropie <hendrowibowo@msn.com>
2020-08-03 17:07:21 +0700

    fix(Customer): model update

--- app/Models/Income/Customer.php
+++ app/Models/Income/Customer.php
@@ -23,7 +23,7 @@ class Customer extends Model
 
     protected $fillable = [
         'code', 'name', 'phone', 'fax', 'email', 'address', 'subdistrict', 'district', 'province_id', 'zipcode',
-        'bank_account', 'npwp', 'pkp', 'with_ppn', 'with_pph', 'ppn', 'sen_service', 'exclude_service', 'description', 'enable',
+        'bank_account', 'npwp', 'pkp', 'with_ppn', 'with_pph', 'ppn', 'sen_service', 'exclude_service', 'bounded_service', 'description', 'enable',
         'invoice_mode', 'delivery_mode', 'order_mode', 'order_manual_allowed', 'order_monthly_actived', 'order_lots'
     ];
 
