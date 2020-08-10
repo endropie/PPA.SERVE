5f81fa0308dc0d3496342c2fc43757c59d3a6e88
endropie <hendrowibowo@msn.com>
2020-08-10 16:34:29 +0700

    fix(RequestOrder): invoicing filter.

--- app/Filters/Income/RequestOrder.php
+++ app/Filters/Income/RequestOrder.php
@@ -22,22 +22,27 @@ class RequestOrder extends Filter
         return $this->builder->where('date', '<=',  $value);
     }
 
+    public function invoicing($order = 'true') {
+        return $this->builder
+            ->where('status', 'CLOSED')
+            ->whereHas('delivery_orders', function($q) {
+                return $q->whereNull('acc_invoice_id');
+            });
+    }
+
     public function sort_counter_invoiced($order = '') {
-        // abort(501, 'ini aku');
         return $this->builder->select('request_orders.*',
             \DB::raw("(SELECT COUNT(*) FROM delivery_orders WHERE request_orders.id = delivery_orders.request_order_id AND delivery_orders.acc_invoice_id IS NOT NULL) as fieldsort"))
         ->orderBy('fieldsort', $order);
     }
 
     public function sort_counter_confirmed($order = '') {
-        // abort(501, 'ini aku');
         return $this->builder->select('request_orders.*',
             \DB::raw("(SELECT COUNT(*) FROM delivery_orders WHERE request_orders.id = delivery_orders.request_order_id AND delivery_orders.acc_invoice_id IS NULL AND status = 'CONFIRMED') as fieldsort"))
         ->orderBy('fieldsort', $order);
     }
 
     public function sort_counter_delivered($order = '') {
-        // abort(501, 'ini aku');
         return $this->builder->select('request_orders.*',
             \DB::raw("(SELECT COUNT(*) FROM delivery_orders WHERE request_orders.id = delivery_orders.request_order_id AND delivery_orders.acc_invoice_id IS NULL AND status <> 'CONFIRMED') as fieldsort"))
         ->orderBy('fieldsort', $order);
