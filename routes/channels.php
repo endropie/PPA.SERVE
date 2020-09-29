7e8f3502b84742a9807e7ec6f598524d8b7ff885
endropie <hendrowibowo@msn.com>
2020-09-29 11:28:40 +0700

    fix(route): clear not to use

--- routes/channels.php
+++ routes/channels.php
@@ -14,11 +14,3 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-
-Broadcast::channel('chat', function ($user) {
-    return $user;
-});
-
-Broadcast::channel('realtime', function ($data) {
-    return $data;
-});
