03bb502987650422227a818d651166e9cba6fdb5
endropie <hendrowibowo@msn.com>
2021-06-29 13:47:05 +0700

    add fillable "delivery_over_allowed" of customer

--- app/Models/Income/Customer.php
+++ app/Models/Income/Customer.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models\Income;
 
 use App\Models\Model;
@@ -15,11 +16,11 @@ class Customer extends Model
         'code', 'name', 'phone', 'fax', 'email', 'address', 'subdistrict', 'district', 'province_id', 'zipcode',
         'bank_account', 'npwp', 'pkp', 'with_ppn', 'with_pph', 'ppn', 'sen_service', 'exclude_service', 'bounded_service', 'description', 'enable',
         'invoice_mode', 'invoice_request_required', 'invoice_category_price',
-        'delivery_mode', 'delivery_manual_allowed',
+        'delivery_mode', 'delivery_manual_allowed', 'delivery_over_allowed',
         'order_mode', 'partialidate_allowed', 'order_manual_allowed', 'order_monthly_actived', 'order_lots'
     ];
 
-    protected $appends = [ 'address_raw', 'is_invoice_request' ];
+    protected $appends = ['address_raw', 'is_invoice_request'];
 
     protected $hidden = ['created_at', 'updated_at'];
 
@@ -50,11 +51,13 @@ class Customer extends Model
         return $this->hasMany('App\Models\Income\DeliveryTask');
     }
 
-    public function customer_items() {
+    public function customer_items()
+    {
         return $this->hasMany('App\Models\Common\Item');
     }
 
-    public function category_item_prices() {
+    public function category_item_prices()
+    {
         return $this->hasMany('App\Models\Common\CategoryItemPrice');
     }
 
@@ -63,17 +66,19 @@ class Customer extends Model
         return $this->belongsTo('App\Models\Reference\Province');
     }
 
-    public function getAddressRawAttribute() {
+    public function getAddressRawAttribute()
+    {
         $raw  = ($this->address ?? '');
-        $raw .= ($this->subdistrict ? "\n". $this->subdistrict .' ' : '');
-        $raw .= ($this->district ?  $this->district .', ' : '');
-        $raw .= ($this->province_id ? "\n". $this->province()->value('name') .' ' : '');
-        $raw .= ($this->zipcode ? ' '. $this->zipcode : '');
+        $raw .= ($this->subdistrict ? "\n" . $this->subdistrict . ' ' : '');
+        $raw .= ($this->district ?  $this->district . ', ' : '');
+        $raw .= ($this->province_id ? "\n" . $this->province()->value('name') . ' ' : '');
+        $raw .= ($this->zipcode ? ' ' . $this->zipcode : '');
 
         return $raw;
     }
 
-    public function getIsInvoiceRequestAttribute() {
+    public function getIsInvoiceRequestAttribute()
+    {
         if ($this->order_mode != 'NONE') return false;
 
         return $this->invoice_request_required;
@@ -85,22 +90,22 @@ class Customer extends Model
         return $this->trips()->where('date', $date)->get();
     }
 
-    protected  static function booted ()
+    protected  static function booted()
     {
-        static::registerModelEvent('accurate.pushing', function($model, $record) {
+        static::registerModelEvent('accurate.pushing', function ($model, $record) {
             return array_merge($record, [
                 'name' => (string) $model->name,
                 'customerNo' => (string) $model->code,
                 'notes' => (string) $model->description,
-                'pkpNo' => (string) $model->pkp ,
-                'npwpNo' => (string) $model->npwp ,
+                'pkpNo' => (string) $model->pkp,
+                'npwpNo' => (string) $model->npwp,
                 'billStreet' => (string) $model->address_raw,
                 // 'billCity' => (string) $model->district ,
                 // 'billProvince' => (string) $model->province ? $model->province->name : null,
-                'billZipCode' => (string) $model->zipcode ,
-                'workPhone' => (string) $model->phone ,
-                'fax' => (string) $model->fax ,
-                'email' => (string) $model->email ,
+                'billZipCode' => (string) $model->zipcode,
+                'workPhone' => (string) $model->phone,
+                'fax' => (string) $model->fax,
+                'email' => (string) $model->email,
             ]);
         });
     }
